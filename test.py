8f3219121a1bd9a9a0609c918b5af8477e82d63f
HotakShin <hotakshin@gmail.com>
2021-04-25 21:52:48 +0900

    upbit autu-trading

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyupbit
-
-access = "eCZGW6UBUfGEgrv5Au7Y7OWFuP61FvtyZ7zbx0NF"          # 본인 값으로 변경
-secret = "RXTgeFQyCRDu2x0OomOTYiQGuEWxf01gV25ucmPf"          # 본인 값으로 변경
-upbit = pyupbit.Upbit(access, secret)
-
-print(upbit.get_balance("KRW-XRP"))     # KRW-XRP 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
